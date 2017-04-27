bff1132d3a8ca437d6a7b975eda53cb4fbff12b2
Christophe GERARDIN <christophe.gerardin@j2i.fr>
2017-04-27 16:43:41 +0200

    Empêche de redimensionner la fenêtre de connexion

--- SharpGED client/Forms/LoginForm.Designer.cs
+++ SharpGED client/Forms/LoginForm.Designer.cs
@@ -43,10 +43,9 @@
             // 
             // ButtonConnect
             // 
-            this.ButtonConnect.Location = new System.Drawing.Point(467, 445);
-            this.ButtonConnect.Margin = new System.Windows.Forms.Padding(7);
+            this.ButtonConnect.Location = new System.Drawing.Point(200, 199);
             this.ButtonConnect.Name = "ButtonConnect";
-            this.ButtonConnect.Size = new System.Drawing.Size(175, 51);
+            this.ButtonConnect.Size = new System.Drawing.Size(75, 23);
             this.ButtonConnect.TabIndex = 11;
             this.ButtonConnect.Text = "Connexion";
             this.ButtonConnect.UseVisualStyleBackColor = true;
@@ -54,69 +53,77 @@
             // 
             // TextBoxServer
             // 
-            this.TextBoxServer.Location = new System.Drawing.Point(193, 221);
+            this.TextBoxServer.Location = new System.Drawing.Point(83, 99);
+            this.TextBoxServer.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TextBoxServer.Name = "TextBoxServer";
-            this.TextBoxServer.Size = new System.Drawing.Size(380, 35);
+            this.TextBoxServer.Size = new System.Drawing.Size(165, 20);
             this.TextBoxServer.TabIndex = 12;
             this.TextBoxServer.Text = "localhost";
             // 
             // TextBoxUser
             // 
-            this.TextBoxUser.Location = new System.Drawing.Point(193, 309);
+            this.TextBoxUser.Location = new System.Drawing.Point(83, 139);
+            this.TextBoxUser.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TextBoxUser.Name = "TextBoxUser";
-            this.TextBoxUser.Size = new System.Drawing.Size(449, 35);
+            this.TextBoxUser.Size = new System.Drawing.Size(195, 20);
             this.TextBoxUser.TabIndex = 13;
             this.TextBoxUser.Text = "root";
             // 
             // TextBoxPassword
             // 
-            this.TextBoxPassword.Location = new System.Drawing.Point(193, 364);
+            this.TextBoxPassword.Location = new System.Drawing.Point(83, 163);
+            this.TextBoxPassword.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TextBoxPassword.Name = "TextBoxPassword";
             this.TextBoxPassword.PasswordChar = '●';
-            this.TextBoxPassword.Size = new System.Drawing.Size(449, 35);
+            this.TextBoxPassword.Size = new System.Drawing.Size(195, 20);
             this.TextBoxPassword.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 224);
+            this.label1.Location = new System.Drawing.Point(32, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 16;
             this.label1.Text = "Serveur";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 315);
+            this.label2.Location = new System.Drawing.Point(22, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 17;
             this.label2.Text = "Utilisateur";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 367);
+            this.label3.Location = new System.Drawing.Point(6, 165);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 29);
+            this.label3.Size = new System.Drawing.Size(71, 13);
             this.label3.TabIndex = 18;
             this.label3.Text = "Mot de passe";
             // 
             // TextBoxPort
             // 
-            this.TextBoxPort.Location = new System.Drawing.Point(579, 221);
+            this.TextBoxPort.Location = new System.Drawing.Point(248, 99);
+            this.TextBoxPort.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TextBoxPort.Name = "TextBoxPort";
-            this.TextBoxPort.Size = new System.Drawing.Size(63, 35);
+            this.TextBoxPort.Size = new System.Drawing.Size(29, 20);
             this.TextBoxPort.TabIndex = 19;
             this.TextBoxPort.Text = "9090";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(4, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(2, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(655, 205);
+            this.pictureBox1.Size = new System.Drawing.Size(281, 92);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 20;
             this.pictureBox1.TabStop = false;
@@ -124,9 +131,9 @@
             // LoginForm
             // 
             this.AcceptButton = this.ButtonConnect;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 512);
+            this.ClientSize = new System.Drawing.Size(282, 230);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.TextBoxPort);
             this.Controls.Add(this.label3);
@@ -136,6 +143,8 @@
             this.Controls.Add(this.TextBoxUser);
             this.Controls.Add(this.TextBoxServer);
             this.Controls.Add(this.ButtonConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
